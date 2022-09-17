64a2a994c96aae71254143912ed37d7e149dcaf0
Rafael <faelmota7@gmail.com>
2022-09-16 22:11:40 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Média Aritmética de 3 números\n");
+
+
+Console.Write("Digite o primeiro número: ");
+double numero1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+double numero2 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.Write("Digite o terceiro número: ");
+double numero3 = Convert.ToDouble(Console.ReadLine());
+
+double media = (numero1 + numero2 + numero3) / 3;
+
+
+Console.WriteLine($"\nMédia: {media:N1}");
